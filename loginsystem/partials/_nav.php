28ed1cd3d0cce3fe568b7c7eed8fbd49bb9d6de3
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-07-14 17:09:39 +0530

    updated the navbar according to the session

--- loginsystem/partials/_nav.php
+++ loginsystem/partials/_nav.php
@@ -5,8 +5,8 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
 else{
   $loggedin = false;
 }
-// echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-//   <a class="navbar-brand" href="/loginsystem">iSecure</a>
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/loginsystem">iSecure</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
