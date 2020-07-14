7430bb276e657cf664b5644763f2152134eab993
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-07-14 17:11:52 +0530

    updated the navbar according to the session

--- loginsystem/partials/_nav.php
+++ loginsystem/partials/_nav.php
@@ -37,6 +37,6 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-//   </div>
+  </div>
 // </nav>';
 ?>
