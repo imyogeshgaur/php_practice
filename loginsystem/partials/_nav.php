aeaba518f4c97c1abd37c27763f56f710a8a0201
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-07-14 17:10:57 +0530

    updated the navbar according to the session

--- loginsystem/partials/_nav.php
+++ loginsystem/partials/_nav.php
@@ -25,11 +25,11 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="nav-link" href="/loginsystem/signup.php">Signup</a>
       </li>';
       }
-//       if($loggedin){
+      if($loggedin){
       echo '<li class="nav-item">
         <a class="nav-link" href="/loginsystem/logout.php">Logout</a>
       </li>';
-//     }
+    }
        
       
 //     echo '</ul>
