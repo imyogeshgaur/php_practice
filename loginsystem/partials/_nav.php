b349ac8d7409b171921bb7f828a985c05c79120d
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-07-14 17:09:49 +0530

    updated the navbar according to the session

--- loginsystem/partials/_nav.php
+++ loginsystem/partials/_nav.php
@@ -11,11 +11,11 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-//   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//     <ul class="navbar-nav mr-auto">
-//       <li class="nav-item active">
-//         <a class="nav-link" href="/loginsystem/welcome.php">Home <span class="sr-only">(current)</span></a>
-//       </li>';
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/loginsystem/welcome.php">Home <span class="sr-only">(current)</span></a>
+      </li>';
 
 //       if(!$loggedin){
 //       echo '<li class="nav-item">
