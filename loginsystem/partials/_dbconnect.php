62de593d9a8d185504978bc1cb1431fcfb41d15b
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-07-14 15:40:32 +0530

    connect the database sucessfully

--- loginsystem/partials/_dbconnect.php
+++ loginsystem/partials/_dbconnect.php
@@ -5,7 +5,8 @@ $username ='root';
 $password ='';
 $database = 'users';
 
-$conn == mysqli_connect($server,$username,$password,$database);
+$conn = mysqli_connect($server,$username,$password,$database);
+
 if($conn){
     echo "Success";
 }
